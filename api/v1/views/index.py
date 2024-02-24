aa837f2752bd7979c3fed8f52831c63fae438eb1
StephenAnthony8 <anthonysteve02@gmail.com>
2024-02-24 05:36:44 +0300

    q4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,16 @@
 """views blueprint implementation"""
 from api.v1.views import app_views
 from flask import jsonify
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
+
+classes = {"amenities": Amenity, "cities": City,
+           "places": Place, "reviews": Review, "states": State, "users": User}
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -12,3 +22,14 @@ def return_status():
         }
 
     return (jsonify(j_encode))
+
+
+@app_views.route('/stats', strict_slashes=False)
+def return_stats():
+    """returns a JSON representation of all objects"""
+    dict_obj = {}
+
+    for key, value in classes.items():
+        dict_obj[key] = storage.count(value)
+
+    return (jsonify(dict_obj))
