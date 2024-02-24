841338d9705f50ba46aff7c4168c3f6c522ea57e
StephenAnthony8 <anthonysteve02@gmail.com>
2024-02-24 04:12:00 +0300

    q3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+"""app_views instantiation & index imports"""
 # module documentation
 from flask import Blueprint
 
 # url prefix must be '/api/v1'
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-if (True):
-    from api.v1.views.index import *  # wildcard import of everything in index
+from api.v1.views.index import *  # wildcard import of everything in index
