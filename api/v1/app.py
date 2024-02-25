592da2a06ebb56ad8e343cc2c2be196870296261
StephenAnthony8 <anthonysteve02@gmail.com>
2024-02-25 17:03:14 +0300

    idk if this will fix it

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """starts a Flask web application"""
-from flask import Flask, jsonify
-from flask_cors import CORS
-from os import getenv
 from models import storage
 from api.v1.views import app_views
+from os import getenv
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
