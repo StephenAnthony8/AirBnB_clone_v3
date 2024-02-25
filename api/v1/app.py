ce03642142cfba6b51e1fd7bfab748c4210b7223
StephenAnthony8 <anthonysteve02@gmail.com>
2024-02-25 17:38:20 +0300

    idk if this will fix it

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,27 +2,28 @@
 """starts a Flask web application"""
 from models import storage
 from api.v1.views import app_views
-from os import getenv
 from flask import Flask, jsonify
 from flask_cors import CORS
+from os import getenv
 
 app = Flask(__name__)
+
+# CORS config
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 # register blueprint here
 app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown_my_db(exc):
+    """closes the storage on end of connection to host"""
+    storage.close()
 
 
 @app.errorhandler(404)
 def page_not_found(e):
     """returns an error 404 JSON"""
     return (jsonify({'error': 'Not found'}), 404)
-
-
-@app.teardown_appcontext
-def teardown_my_db(exc):
-    """closes the storage on end of connection to host"""
-    storage.close()
 
 
 if __name__ == '__main__':
