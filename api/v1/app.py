18667999e8002297a1871d727bee384d03e4cbbb
StephenAnthony8 <anthonysteve02@gmail.com>
2024-02-26 18:19:12 +0300

    readme inclusion

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """starts a Flask web application"""
-from models import storage
 from api.v1.views import app_views
+from models import storage
 from flask import Flask, jsonify
 from flask_cors import CORS
 from os import getenv
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 """app_views instantiation & index imports"""
 from flask import Blueprint
 
